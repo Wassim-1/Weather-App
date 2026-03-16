52aa97f628d16cc9d0ed3473f71b24007f13e4b9
Wassim-1 <harrabiwassim503@gmail.com>
2026-03-16 15:17:39 +0100

    Added API calls to get details about the city.

---
+++ config_example.py
@@ -0,0 +1,2 @@
+api_key="YOUR_API_KEY"
+base_url="https://api.openweathermap.org/data/2.5/weather"
---
+++ gui.py
@@ -0,0 +1,15 @@
+from tkinter import *
+window=Tk()
+window.geometry("500x500")
+window.title("Weather")
+window.config(background="gray")
+label=Label(window,text="Welcome to weather app",
+            font=('Arial',20,'bold'),
+            width=25,height=2,
+            fg="#f30707",
+            bg='#180e45',
+            relief='sunken',
+            bd=10
+            )
+label.pack()
+window.mainloop()
